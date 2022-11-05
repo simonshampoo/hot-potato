84e719bc935966e2c8c8539f5ba144b01cace083
Simon Shamoon <simonshm5@gmail.com>
2022-11-05 16:58:07 -0500

    testing...

--- test/HotPotato.t.sol
+++ test/HotPotato.t.sol
@@ -1,10 +1,11 @@
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
-contract CounterTest is Test {
+contract HotPotatoTest is Test {
     Counter public counter;
 
     function setUp() public {
