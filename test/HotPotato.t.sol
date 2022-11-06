59d68b20e4eb01dea6a71c613a0a828667760f48
Simon Shamoon <simonshm5@gmail.com>
2022-11-05 22:08:49 -0500

    my tests arent working && git pus
    h
    h

--- test/HotPotato.t.sol
+++ test/HotPotato.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Vm.sol";
 import "forge-std/Test.sol";
 import "../src/HotPotato.sol";
+import "forge-std/Vm.sol";
 
 contract HotPotatoTest is Test {
     HotPotato hotPotato;
@@ -12,7 +13,18 @@ contract HotPotatoTest is Test {
         hotPotato = new HotPotato();
     }
 
-    function testSend() public {}
+    function testSend() public {
+        address sender = address(1);
+        address receiver = address(2);
+        vm.startPrank(sender);
+
+        vm.deal(sender, 1 ether);
+
+        hotPotato.heatPotato(payable(receiver));
+        vm.stopPrank();
+        assertTrue(receiver.balance == 1 ether);
+        assertTrue(sender.balance == 0 ether);
+    }
 
     //    function testIncrement() public {
     //        assertEq(counter.number(), 1);
