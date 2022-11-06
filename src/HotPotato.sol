ea2ac2e1d83f5da1c632fe9b5ab74a73970aa0ff
Simon Shamoon <simonshm5@gmail.com>
2022-11-05 23:32:54 -0500

    why are my tests not working helloooo

--- src/HotPotato.sol
+++ src/HotPotato.sol
@@ -1,27 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Martyr} from "./Martyr.sol";
+import { Martyr } from "./Martyr.sol";
 
 contract HotPotato {
+    event Deployed(address to, uint256 amount);
 
-    event Deployed(address to, uint256 amount); 
-
-    function getCreationBytecode(address to) public pure returns (bytes memory) {
+    function getCreationBytecode(
+        address to
+    ) public pure returns (bytes memory) {
         bytes memory bytecode = type(Martyr).creationCode;
 
-        return abi.encodePacked(bytecode, abi.encode(to));  
+        return abi.encodePacked(bytecode, abi.encode(to));
     }
 
-    function heatPotato(address payable to) payable external returns (address martyr) {
-        bytes memory  code = getCreationBytecode(to); 
-
-        assembly {
-            let amtToSend := callvalue()
-            martyr := create(amtToSend, code, 63) 
-    
-        }        
+    function heatPotato(
+        address payable to
+    ) external payable returns (address martyr) {
+        bytes memory code = getCreationBytecode(to);
+        //assembly {
+        //    let amtToSend := callvalue()
+        //    martyr := create(amtToSend, code, 63)
+        //}
+        
+        Martyr m = new Martyr.value(msg.value)(to); 
+        
         emit Deployed(martyr, msg.value);
     }
-
 }
