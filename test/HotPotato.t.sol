696b224856fe4e4cb57bd2c18b59a379e8324c9a
Simon Shamoon <simonshm5@gmail.com>
2022-11-05 19:50:41 -0500

    writing tests

--- test/HotPotato.t.sol
+++ test/HotPotato.t.sol
@@ -2,19 +2,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/Vm.sol";
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import "../src/HotPotato.sol";
 
 contract HotPotatoTest is Test {
-    Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        hotPotato = new HotPotato(); 
+    }    
+    function setUp() public {
     }
+    
+    function testSend() public {
+        
 
     function testIncrement() public {
-        counter.increment();
         assertEq(counter.number(), 1);
     }
 
